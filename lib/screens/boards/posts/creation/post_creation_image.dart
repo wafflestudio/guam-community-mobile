7f0d85db59bca7f3efe6d039dda544a622d1773d
yeonghyeonKO <dk02315@gmail.com>
2021-12-28 00:39:13 +0900

    change cancel_filled icon

--- lib/screens/boards/posts/creation/post_creation_image.dart
+++ lib/screens/boards/posts/creation/post_creation_image.dart
@@ -121,10 +121,10 @@ class _PostCreationImageState extends State<PostCreationImage> {
                                   ),
                                 ),
                                 Positioned(
-                                  top: 4,
-                                  right: 4,
+                                  top: 2,
+                                  right: 2,
                                   child: IconButton(
-                                    iconSize: 18,
+                                    iconSize: 23,
                                     padding: EdgeInsets.zero,
                                     constraints: BoxConstraints(),
                                     icon: SvgPicture.asset('assets/icons/cancel_filled.svg'),
