225f96643b3266427e626dd073009327127c6ed0
yeonghyeonKO <dk02315@gmail.com>
2022-01-13 15:53:02 +0900

    add animation effect on stars and text

--- lib/screens/app/splash/splash_screen.dart
+++ lib/screens/app/splash/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:guam_community_client/helpers/svg_provider.dart';
 import 'package:guam_community_client/screens/app/splash/splash_text.dart';
 import 'package:guam_community_client/styles/colors.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class SplashScreen extends StatefulWidget {
   @override _SplashScreenState createState() => _SplashScreenState();
@@ -9,28 +10,21 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
   AnimationController _animationController;
-  Animation<Offset> _animation;
+
+  _setAnimation(double begin, double end){
+    return CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(begin, end, curve: Curves.fastOutSlowIn)
+    );
+  }
 
   @override
   void initState() {
     super.initState();
-
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 900),
-    )..forward();
-
-    _animation = Tween<Offset>(
-        begin: Offset(2.0, 0.0),
-        end: Offset(0.0, 1.0)
-    ).animate(CurvedAnimation(
-        parent: _animationController,
-        curve: Interval(
-          1/9,
-          1,
-          curve: Curves.linear,
-        )
-    ));
+      duration: const Duration(milliseconds: 1500),
+    )..repeat();
   }
 
   @override
@@ -40,6 +34,8 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
   }
 
   @override Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: Stack(
         children: [
@@ -50,7 +46,7 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
               image: DecorationImage(
                 fit: BoxFit.fitWidth,
                 alignment: Alignment.bottomCenter,
-                image: AssetImage('assets/backgrounds/back_0.75x.png')
+                image: AssetImage('assets/backgrounds/back_0.75x.png'),
               )
             ),
           ),
@@ -61,36 +57,75 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
               image: DecorationImage(
                 fit: BoxFit.fitWidth,
                 alignment: Alignment.bottomCenter,
-                image: AssetImage('assets/backgrounds/front_0.75x.png')
+                image: AssetImage('assets/backgrounds/front_0.75x.png'),
               )
             ),
             child: Stack(
               children: [
-                SlideTransition(
-                  position: _animation,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: SvgProvider('assets/backgrounds/splash/Subtract-3.svg'),
-                      ),
-                    )
-                  )
+                // 위에서부터 아래로 star 배치
+                _starSplash(
+                  begin: 1/4, end: 1, // animation
+                  width: 26, height: 26, // size of star
+                  top: size.height*0.12, left: size.width*0.24, // position of star (iPhone 13 : 375x812)
+                  color: GuamColorFamily.purpleDark1,
                 ),
-                SlideTransition(
-                  position: _animation,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: SvgProvider('assets/backgrounds/splash/Subtract-2.svg'),
-                      ),
-                    )
-                  )
+                _starSplash(
+                  begin: 1/20, end: 1,
+                  width: 32, height: 32,
+                  top: size.height*0.2, left: size.width*0.88,
+                  color: GuamColorFamily.purpleCore,
+                ),
+                _starSplash(
+                  begin: 1/10, end: 1,
+                  width: 25, height: 25,
+                  top: size.height*0.32, left: size.width*0.42,
+                  color: GuamColorFamily.purpleLight1,
+                ),
+                _starSplash(
+                  begin: 1/2, end: 1,
+                  width: 25, height: 25,
+                  top: size.height*0.45, left: size.width*0.78,
+                  color: GuamColorFamily.purpleLight2,
+                ),
+                _starSplash(
+                  begin: 1/2, end: 1,
+                  width: 32, height: 32,
+                  top: size.height*0.56, left: size.width*0.16,
+                  color: GuamColorFamily.purpleLight2,
+                ),
+                _starSplash(
+                  begin: 1/30, end: 1,
+                  width: 25, height: 25,
+                  top: size.height*0.66, left: size.width*0.57,
+                  color: GuamColorFamily.purpleLight3,
                 ),
                 SplashText(),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _starSplash({double top, double left, double width, double height, double begin, double end, HexColor color}){
+    return Positioned(
+      top: top,
+      left: left,
+      child: ScaleTransition(
+        scale: _setAnimation(begin, end),
+        child: Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: SvgProvider(
+                'assets/backgrounds/splash/star_splash.svg',
+                color: color,
+              ),
+            ),
+          )
+        )
       ),
     );
   }
--- lib/screens/app/splash/splash_text.dart
+++ lib/screens/app/splash/splash_text.dart
@@ -1,30 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:guam_community_client/styles/colors.dart';
 
-class SplashText extends StatelessWidget {
+
+// ScaleTransition(
+// scale: _setAnimation(begin, end),
+// child: Container(
+// width: width,
+// height: height,
+// decoration: BoxDecoration(
+// image: DecorationImage(
+// image: SvgProvider(
+// 'assets/backgrounds/splash/star_splash.svg',
+// color: color,
+// ),
+// ),
+// )
+// )
+// ),
+
+class SplashText extends StatefulWidget {
+  @override
+  State<SplashText> createState() => _SplashTextState();
+}
+
+class _SplashTextState extends State<SplashText> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-              'IT인들의 커뮤니티',
-              style: TextStyle(
-                fontSize: 18,
-                color: GuamColorFamily.grayscaleWhite,
-              )
-          ),
-          Text(
-              'Guam',
-              style: TextStyle(
-                fontSize: 56,
-                fontFamily: "Poppins",
-                fontWeight: FontWeight.w700,
-                color: GuamColorFamily.purpleDark1,
-              )
-          )
-        ],
+    return ScaleTransition(
+      scale: CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(1/100, 1, curve: Curves.fastOutSlowIn)
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                'IT인들의 커뮤니티',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: GuamColorFamily.grayscaleWhite,
+                )
+            ),
+            Text(
+                'Guam',
+                style: TextStyle(
+                  fontSize: 56,
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w700,
+                  color: GuamColorFamily.purpleDark1,
+                )
+            )
+          ],
+        ),
       ),
     );
   }
