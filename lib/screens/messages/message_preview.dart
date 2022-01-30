7ae8be4fe50e8c5d29368c8f4b34d14af248c5c2
yeonghyeonKO <dk02315@gmail.com>
2022-01-30 17:39:28 +0900

    change boxShadow of message-preview

--- lib/screens/messages/message_preview.dart
+++ lib/screens/messages/message_preview.dart
@@ -18,122 +18,120 @@ class MessagePreview extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-      child: Card(
-        margin: EdgeInsets.zero,
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.1),
-                offset: Offset(0, -2),
-              ),
-            ],
-          ),
-          padding: EdgeInsets.only(left: 12, top: 6, bottom: 6),
-          child: InkWell(
-            onTap: () {
-              // 쪽지함 수정 페이지에서는 쪽지 클릭 후 이동 불가능
-              if (!editable)
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (_) => MessageDetail(
-                    messages,
-                    messageBox.otherProfile.nickname,
-                  ))
-                );
-            },
-            child: Row(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: messageBox.otherProfile.profileImg != null
-                              ? NetworkImage(messageBox.otherProfile.profileImg.urlPath)
-                              : SvgProvider('assets/icons/profile_image.svg')
-                        ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 1,
+              blurRadius: 2,
+            ),
+          ],
+        ),
+        padding: EdgeInsets.only(left: 12, top: 6, bottom: 6),
+        child: InkWell(
+          onTap: () {
+            // 쪽지함 수정 페이지에서는 쪽지 클릭 후 이동 불가능
+            if (!editable)
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => MessageDetail(
+                  messages,
+                  messageBox.otherProfile.nickname,
+                ))
+              );
+          },
+          child: Row(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: messageBox.otherProfile.profileImg != null
+                            ? NetworkImage(messageBox.otherProfile.profileImg.urlPath)
+                            : SvgProvider('assets/icons/profile_image.svg')
                       ),
                     ),
-                    if (!messageBox.isRead)
-                      Positioned(
-                        top: 0,
-                        child: CircleAvatar(
-                          backgroundColor: GuamColorFamily.fuchsiaCore,
-                          radius: 6,
-                        )
-                      ),
-                  ],
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.64,
-                  padding: EdgeInsets.only(left: 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        messageBox.otherProfile.nickname,
-                        style: TextStyle(
-                          fontSize: 12,
-                          height: 1.6,
-                          fontFamily: GuamFontFamily.SpoqaHanSansNeoMedium,
-                          color: GuamColorFamily.grayscaleGray2,
-                        ),
-                      ),
-                      Text(
-                        messageBox.lastContent,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 12,
-                          height: 1.6,
-                          fontFamily: GuamFontFamily.SpoqaHanSansNeoRegular,
-                          color: messageBox.isRead
-                              ? GuamColorFamily.grayscaleGray4
-                              : GuamColorFamily.grayscaleGray2,
-                        ),
-                      ),
-                    ],
                   ),
-                ),
-                Spacer(),
-                if (editable)
-                  TextButton(
-                    onPressed: (){},
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      minimumSize: Size(23, 20),
+                  if (!messageBox.isRead)
+                    Positioned(
+                      top: 0,
+                      child: CircleAvatar(
+                        backgroundColor: GuamColorFamily.fuchsiaCore,
+                        radius: 6,
+                      )
                     ),
-                    child: Text(
-                      '삭제',
+                ],
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.64,
+                padding: EdgeInsets.only(left: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      messageBox.otherProfile.nickname,
                       style: TextStyle(
                         fontSize: 12,
+                        height: 1.6,
                         fontFamily: GuamFontFamily.SpoqaHanSansNeoMedium,
-                        color: GuamColorFamily.redCore,
+                        color: GuamColorFamily.grayscaleGray2,
                       ),
                     ),
+                    Text(
+                      messageBox.lastContent,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 12,
+                        height: 1.6,
+                        fontFamily: GuamFontFamily.SpoqaHanSansNeoRegular,
+                        color: messageBox.isRead
+                            ? GuamColorFamily.grayscaleGray4
+                            : GuamColorFamily.grayscaleGray2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              if (editable)
+                TextButton(
+                  onPressed: (){},
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    minimumSize: Size(23, 20),
                   ),
-                if (!editable)
-                Padding(
-                  padding: EdgeInsets.only(right: 10, top: 14, bottom: 15),
                   child: Text(
-                    (DateTime.now().minute - messageBox.createdAt.minute).toString() + "분 전",
+                    '삭제',
                     style: TextStyle(
                       fontSize: 12,
-                      height: 1.6,
-                      fontFamily: GuamFontFamily.SpoqaHanSansNeoRegular,
-                      color: GuamColorFamily.grayscaleGray4,
+                      fontFamily: GuamFontFamily.SpoqaHanSansNeoMedium,
+                      color: GuamColorFamily.redCore,
                     ),
                   ),
                 ),
-              ],
-            ),
+              if (!editable)
+              Padding(
+                padding: EdgeInsets.only(right: 10, top: 14, bottom: 15),
+                child: Text(
+                  (DateTime.now().minute - messageBox.createdAt.minute).toString() + "분 전",
+                  style: TextStyle(
+                    fontSize: 12,
+                    height: 1.6,
+                    fontFamily: GuamFontFamily.SpoqaHanSansNeoRegular,
+                    color: GuamColorFamily.grayscaleGray4,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
