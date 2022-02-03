7c996d0ac1d721cbc12c272de9d7d06467bb7f74
yeonghyeonKO <dk02315@gmail.com>
2022-02-04 01:09:00 +0900

    separate profile_body into two

---
+++ lib/screens/profiles/other_profiles_body.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:guam_community_client/models/profiles/profile.dart';
+import 'package:guam_community_client/screens/profiles/buttons/message_send_button.dart';
+import 'package:guam_community_client/screens/profiles/profile/profile_img.dart';
+import 'package:guam_community_client/screens/profiles/profile/profile_intro.dart';
+import 'package:guam_community_client/screens/profiles/profile/profile_nickname.dart';
+import 'package:guam_community_client/screens/profiles/profile/profile_skillset.dart';
+import 'package:guam_community_client/screens/profiles/profile/profile_web_buttons.dart';
+
+class OtherProfilesBody extends StatelessWidget {
+  final Profile profile;
+  final bool isMine;
+
+  const OtherProfilesBody({this.profile, this.isMine});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.fromLTRB(24, 40, 24, 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ProfileImg(profileImg: profile.profileImg, height: 144, width: 144),
+          ProfileNickname(nickname: profile.nickname, isMine: isMine),
+          ProfileIntro(profile.intro),
+          ProfileWebButtons(
+            githubId: profile.githubId,
+            blogUrl: profile.blogUrl,
+            isMine: isMine,
+          ),
+          // 추후 MyProfile의 id랑 비교해서 본인임이 확인되면 프로필 탭으로 이동하도록 하겠습니다.
+          if (!isMine) MessageSendButton(profile),
+          ProfileSkillSet(profile.skillSet),
+        ],
+      ),
+    );
+  }
+}
