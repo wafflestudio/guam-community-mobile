3f27f2fcd291747f1417cc7d85daa5d7fdc647b0
yeonghyeonKO <dk02315@gmail.com>
2022-03-12 23:34:16 +0900

    attach '작성자' badge next to the name of each comment

--- lib/screens/boards/comments/comment_banner.dart
+++ lib/screens/boards/comments/comment_banner.dart
@@ -3,8 +3,14 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:guam_community_client/commons/common_img_nickname.dart';
 import 'package:guam_community_client/models/boards/comment.dart';
 import 'package:guam_community_client/screens/boards/comments/comment_more.dart';
+import 'package:guam_community_client/screens/boards/posts/preview/post_preview_relative_time.dart';
 import 'package:guam_community_client/styles/colors.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:provider/provider.dart';
+
+import '../../../models/profiles/profile.dart';
+import '../../../providers/user_auth/authenticate.dart';
+import '../../../styles/fonts.dart';
 
 class CommentBanner extends StatelessWidget {
   final Comment comment;
@@ -13,6 +19,8 @@ class CommentBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Profile me = context.read<Authenticate>().me;
+
     return Padding(
       padding: EdgeInsets.only(top: 10),
       child: Row(
@@ -23,6 +31,23 @@ class CommentBanner extends StatelessWidget {
             nickname: comment.profile.nickname,
             nicknameColor: GuamColorFamily.grayscaleGray3,
           ),
+          if (comment.profile.id == me.id)
+            Container(
+              margin: EdgeInsets.only(left: 4),
+              padding: EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+              decoration: BoxDecoration(
+                color: GuamColorFamily.purpleLight3,
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+              child: Text(
+                '작성자',
+                style: TextStyle(
+                  fontSize: 10,
+                  fontFamily: GuamFontFamily.SpoqaHanSansNeoRegular,
+                  color: GuamColorFamily.purpleLight1,
+                ),
+              ),
+            ),
           Spacer(),
           IconButton(
             iconSize: 20,
