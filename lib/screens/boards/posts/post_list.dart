a05b229daaf88ce6d43fc0a6370349762777e28b
yeonghyeonKO <dk02315@gmail.com>
2022-03-29 07:51:07 +0900

    remove unused widget(category filtering in home tab

--- lib/screens/boards/posts/post_list.dart
+++ lib/screens/boards/posts/post_list.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:guam_community_client/providers/posts/posts.dart';
-import 'package:guam_community_client/styles/fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:guam_community_client/providers/posts/posts.dart';
 import 'package:guam_community_client/models/boards/post.dart';
 import 'package:guam_community_client/styles/colors.dart';
 import 'package:guam_community_client/screens/boards/posts/preview/post_preview.dart';
@@ -20,21 +18,6 @@ class PostList extends StatefulWidget {
 }
 
 class _PostListState extends State<PostList> {
-  List<String> selectedCategories = [];
-
-  void setCategory(String category){
-    setState(() => selectedCategories.contains(category)
-        ? selectedCategories.remove(category)
-        : selectedCategories.add(category)
-    );
-  }
-
-  @override
-  void initState() {
-    selectedCategories = ['#개발', '#데이터분석', '#디자인', '#기획/마케팅', '#기타'];
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -56,49 +39,9 @@ class _PostListState extends State<PostList> {
             child: Column(
               children: [...widget.posts.map((post) => PostPreview(post))]
             ),
-          )
+          ),
         ],
       ),
-    );
-  }
-
-  Widget _categoryType(StateSetter myState, String category) {
-    return Builder(
-      builder: (_) => InkWell(
-        onTap: () =>
-          myState(() => selectedCategories.contains(category)
-              ? selectedCategories.remove(category)
-              : selectedCategories.add(category)
-          ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                height: 26,
-                child: Text(
-                  category,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontFamily: GuamFontFamily.SpoqaHanSansNeoRegular,
-                    color: selectedCategories.contains(category)
-                        ? GuamColorFamily.purpleCore
-                        : GuamColorFamily.grayscaleGray3,
-                  )
-                )
-              ),
-              if (selectedCategories.contains(category))
-                IconButton(
-                  onPressed: null,
-                  padding: EdgeInsets.only(right: 8),
-                  constraints: BoxConstraints(),
-                  icon: SvgPicture.asset('assets/icons/check.svg'),
-                ),
-            ],
-          ),
-        ),
-      )
     );
   }
 }
