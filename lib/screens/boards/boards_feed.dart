dc058cdc731398c3455d2f537ea2e0908cd0f5d4
yeonghyeonKO <dk02315@gmail.com>
2022-04-08 18:04:37 +0900

    paginating GET posts

--- lib/screens/boards/boards_feed.dart
+++ lib/screens/boards/boards_feed.dart
@@ -13,15 +13,46 @@ class BoardsFeed extends StatefulWidget {
 }
 
 class _BoardsFeedState extends State<BoardsFeed> {
+  final ScrollController _scrollController = ScrollController();
+  bool turnPage = false;
+
+  Future addPosts() async {
+    if (turnPage)
+      Future.delayed(Duration.zero, () async {
+        int beforePostId = context.read<Posts>().posts.last.id;
+        await context.read<Posts>().addPosts(
+          boardId: widget.boardId,
+          beforePostId: beforePostId,
+        );
+      });
+  }
+
   @override
   void initState() {
     context.read<Posts>().fetchPosts(widget.boardId);
+
+    _scrollController.addListener(() {
+      print(context.read<Posts>().posts.length);
+      turnPage = _scrollController.offset / _scrollController.position.maxScrollExtent >= 0.7
+          ? true : false;
+      if (turnPage && context.read<Posts>().hasNext) addPosts();
+    });
     super.initState();
   }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return context.watch<Posts>().loading
         ? Center(child: CircularProgressIndicator())
-        : SingleChildScrollView(child: PostList(context.read<Posts>().posts));
+        : SingleChildScrollView(
+            controller: _scrollController,
+            child: PostList(context.read<Posts>().posts),
+          );
   }
 }
