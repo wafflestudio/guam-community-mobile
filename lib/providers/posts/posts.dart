3e5d444f292dfad875660b6dc8eb59e448977067
yeonghyeonKO <dk02315@gmail.com>
2022-05-18 09:33:29 +0900

    define like/unlikeComment function in provider

--- lib/providers/posts/posts.dart
+++ lib/providers/posts/posts.dart
@@ -35,6 +35,7 @@ class Posts extends ChangeNotifier with Toast {
   List<Post> get newPosts => _newPosts;
   List<Comment> get comments => _comments;
 
+  /// ==== Posts ====
   Future fetchPosts(int boardId) async {
     loading = true;
     try {
@@ -266,111 +267,6 @@ class Posts extends ChangeNotifier with Toast {
     return successful;
   }
 
-  Future fetchComments(int postId) async {
-    List<Comment> comments;
-    try {
-      loading = true;
-      await HttpRequest()
-          .get(
-        path: "community/api/v1/posts/$postId/comments",
-        authToken: await _authProvider.getFirebaseIdToken(),
-      ).then((response) async {
-        if (response.statusCode == 200) {
-          final jsonUtf8 = decodeKo(response);
-          final List<dynamic> jsonList = json.decode(jsonUtf8)["content"];
-          comments = jsonList.map((e) => Comment.fromJson(e)).toList();
-          loading = false;
-        } else {
-          final jsonUtf8 = decodeKo(response);
-          final String err = json.decode(jsonUtf8)["message"];
-          showToast(success: false, msg: err);
-        }
-      });
-      loading = false;
-    } catch (e) {
-      print(e);
-    } finally {
-      notifyListeners();
-    }
-    return comments;
-  }
-
-  Future<bool> createComment({int postId, Map<String, dynamic> fields, dynamic files}) async {
-    bool successful = false;
-    loading = true;
-
-    try {
-      String authToken = await _authProvider.getFirebaseIdToken();
-
-      if (authToken.isNotEmpty) {
-        await HttpRequest()
-            .postMultipart(
-          path: "community/api/v1/posts/$postId/comments",
-          authToken: authToken,
-          fields: fields,
-          files: files,
-        ).then((response) async {
-          if (response.statusCode == 200) {
-            successful = true;
-            loading = false;
-            showToast(success: true, msg: '댓글을 작성했습니다.');
-          } else {
-            String msg = "알 수 없는 오류가 발생했습니다.";
-            switch (response.statusCode) {
-              case 400: msg = "빈 댓글은 입력할 수 없습니다."; break;
-              case 401: msg = "권한이 없습니다."; break;
-              case 404: msg = "존재하지 않는 글입니다."; break;
-            }
-            showToast(success: false, msg: msg);
-          }
-        });
-        loading = false;
-      }
-    } catch (e) {
-      print(e);
-    } finally {
-      notifyListeners();
-    }
-    return successful;
-  }
-
-  Future<bool> deleteComment({int postId, int commentId}) async {
-    bool successful = false;
-    loading = true;
-
-    try {
-      String authToken = await _authProvider.getFirebaseIdToken();
-
-      if (authToken.isNotEmpty) {
-        await HttpRequest()
-            .delete(
-          path: "community/api/v1/posts/$postId/comments/$commentId",
-          authToken: authToken,
-        ).then((response) async {
-          if (response.statusCode == 200) {
-            fetchComments(postId);
-            showToast(success: true, msg: '댓글을 삭제했습니다.');
-            successful = true;
-          } else {
-            String msg = "알 수 없는 오류가 발생했습니다.";
-            switch (response.statusCode) {
-              case 401: msg = "권한이 없습니다."; break;
-              case 404: msg = "존재하지 않는 댓글입니다."; break;
-              case 409: msg = "이미 삭제된 댓글입니다."; break;
-            }
-            showToast(success: false, msg: msg);
-          }
-        });
-        loading = false;
-      }
-    } catch (e) {
-      print(e);
-    } finally {
-      notifyListeners();
-    }
-    return successful;
-  }
-
   Future<bool> likePost({int postId}) async {
     loading = true;
     bool successful = false;
@@ -493,6 +389,180 @@ class Posts extends ChangeNotifier with Toast {
               case 401: msg = "권한이 없습니다."; break;
               case 404: msg = "존재하지 않는 게시글입니다."; break;
               case 409: msg = "이미 스크랩 취소한 글입니다."; break;
+            }
+            showToast(success: false, msg: msg);
+          }
+        });
+        loading = false;
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      notifyListeners();
+    }
+    return successful;
+  }
+
+  /// ==== Comments ====
+  Future fetchComments(int postId) async {
+    List<Comment> comments;
+    try {
+      loading = true;
+      await HttpRequest()
+          .get(
+        path: "community/api/v1/posts/$postId/comments",
+        authToken: await _authProvider.getFirebaseIdToken(),
+      ).then((response) async {
+        if (response.statusCode == 200) {
+          final jsonUtf8 = decodeKo(response);
+          final List<dynamic> jsonList = json.decode(jsonUtf8)["content"];
+          comments = jsonList.map((e) => Comment.fromJson(e)).toList();
+          loading = false;
+        } else {
+          final jsonUtf8 = decodeKo(response);
+          final String err = json.decode(jsonUtf8)["message"];
+          showToast(success: false, msg: err);
+        }
+      });
+      loading = false;
+    } catch (e) {
+      print(e);
+    } finally {
+      notifyListeners();
+    }
+    return comments;
+  }
+
+  Future<bool> createComment({int postId, Map<String, dynamic> fields, dynamic files}) async {
+    bool successful = false;
+    loading = true;
+
+    try {
+      String authToken = await _authProvider.getFirebaseIdToken();
+
+      if (authToken.isNotEmpty) {
+        await HttpRequest()
+            .postMultipart(
+          path: "community/api/v1/posts/$postId/comments",
+          authToken: authToken,
+          fields: fields,
+          files: files,
+        ).then((response) async {
+          if (response.statusCode == 200) {
+            successful = true;
+            loading = false;
+            showToast(success: true, msg: '댓글을 작성했습니다.');
+          } else {
+            String msg = "알 수 없는 오류가 발생했습니다.";
+            switch (response.statusCode) {
+              case 400: msg = "빈 댓글은 입력할 수 없습니다."; break;
+              case 401: msg = "권한이 없습니다."; break;
+              case 404: msg = "존재하지 않는 글입니다."; break;
+            }
+            showToast(success: false, msg: msg);
+          }
+        });
+        loading = false;
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      notifyListeners();
+    }
+    return successful;
+  }
+
+  Future<bool> deleteComment({int postId, int commentId}) async {
+    bool successful = false;
+    loading = true;
+
+    try {
+      String authToken = await _authProvider.getFirebaseIdToken();
+
+      if (authToken.isNotEmpty) {
+        await HttpRequest()
+            .delete(
+          path: "community/api/v1/posts/$postId/comments/$commentId",
+          authToken: authToken,
+        ).then((response) async {
+          if (response.statusCode == 200) {
+            fetchComments(postId);
+            showToast(success: true, msg: '댓글을 삭제했습니다.');
+            successful = true;
+          } else {
+            String msg = "알 수 없는 오류가 발생했습니다.";
+            switch (response.statusCode) {
+              case 401: msg = "권한이 없습니다."; break;
+              case 404: msg = "존재하지 않는 댓글입니다."; break;
+              case 409: msg = "이미 삭제된 댓글입니다."; break;
+            }
+            showToast(success: false, msg: msg);
+          }
+        });
+        loading = false;
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      notifyListeners();
+    }
+    return successful;
+  }
+
+  Future<bool> likeComment({int postId, int commentId}) async {
+    loading = true;
+    bool successful = false;
+    try {
+      String authToken = await _authProvider.getFirebaseIdToken();
+      if (authToken.isNotEmpty) {
+        await HttpRequest()
+            .post(
+          path: "community/api/v1/posts/$postId/comments/$commentId/likes",
+          authToken: authToken,
+        ).then((response) async {
+          if (response.statusCode == 200) {
+            loading = false;
+            successful = true;
+          } else {
+            String msg = "알 수 없는 오류가 발생했습니다.";
+            switch (response.statusCode) {
+              case 401: msg = "권한이 없습니다."; break;
+              case 404: msg = "존재하지 않는 댓글입니다."; break;
+              case 409: msg = "이미 '좋아요'한 댓글입니다."; break;
+            }
+            showToast(success: false, msg: msg);
+          }
+        });
+        loading = false;
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      notifyListeners();
+    }
+    return successful;
+  }
+
+  Future<bool> unlikeComment({int postId, int commentId}) async {
+    loading = true;
+    bool successful = false;
+    try {
+      String authToken = await _authProvider.getFirebaseIdToken();
+      if (authToken.isNotEmpty) {
+        await HttpRequest()
+            .delete(
+          path: "community/api/v1/posts/$postId/comments/$commentId/likes",
+          authToken: authToken,
+        ).then((response) async {
+          if (response.statusCode == 200) {
+            loading = false;
+            successful = true;
+          } else {
+            String msg = "알 수 없는 오류가 발생했습니다.";
+            switch (response.statusCode) {
+              case 401: msg = "권한이 없습니다."; break;
+              case 404: msg = "존재하지 않는 댓글입니다."; break;
+              case 409: msg = "이미 '좋아요' 취소한 댓글입니다."; break;
             }
             showToast(success: false, msg: msg);
           }
