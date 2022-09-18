ba9261b96d3edb54fc8e6549cfad86d7b396c1f0
yeonghyeonKO <dk02315@gmail.com>
2022-09-18 11:19:07 +0900

    block users sending a letter

--- lib/screens/messages/message_detail.dart
+++ lib/screens/messages/message_detail.dart
@@ -138,10 +138,19 @@ class _MessageDetailState extends State<MessageDetail> {
                               //   text: '신고하기',
                               //   onPressed: (){},
                               // ),
-                              // BottomModalDefault(
-                              //   text: '차단하기',
-                              //   onPressed: (){},
-                              // ),
+                              BottomModalWithAlert(
+                                funcName: '차단하기',
+                                title: '${widget.otherProfile!.nickname} 님을 차단하시겠어요?',
+                                body: '사용자를 차단하면, 해당 사용자의 게시글 및 댓글을 확인 할 수 없으며, 서로 쪽지를 주고 받을 수 없습니다.\n\n차단계정 관리는 프로필>계정 설정> 차단 목록 관리 탭에서 확인 가능합니다',
+                                func: () async {
+                                  await context.read<Authenticate>().blockUser(widget.otherProfile!.id)
+                                      .then((successful) {
+                                    if (successful) {
+                                      Navigator.of(context).pop();
+                                    }
+                                  });
+                                },
+                              ),
                             ],
                           ),
                         ),
