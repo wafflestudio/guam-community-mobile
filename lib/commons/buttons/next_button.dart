7396ec68805c5e312cbffe4a3fe1e3628e5b49c3
yeonghyeonKO <dk02315@gmail.com>
2022-11-11 00:42:19 +0900

    component

--- lib/commons/buttons/next_button.dart
+++ lib/commons/buttons/next_button.dart
@@ -16,33 +16,35 @@ class NextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(5, 60, 5, 20),
-      child: InkWell(
-        onTap: onTap as void Function()?,
-        child: Container(
-          height: 56,
-          alignment: Alignment.center,
-          width: page == 1
-              ? MediaQuery.of(context).size.width * 0.9
-              : MediaQuery.of(context).size.width * 0.45,
-          decoration: BoxDecoration(
-            color: active
-                ? GuamColorFamily.purpleCore
-                : GuamColorFamily.grayscaleGray7,
-            borderRadius: BorderRadius.all(Radius.circular(16)),
-          ),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 16,
+    return Padding(
+      padding: EdgeInsets.fromLTRB(5, 40, 5, 20),
+      child: Center(
+        child: InkWell(
+          onTap: onTap as void Function()?,
+          child: Container(
+            height: 56,
+            alignment: Alignment.center,
+            width: page == 1
+                ? MediaQuery.of(context).size.width * 0.9
+                : MediaQuery.of(context).size.width * 0.45,
+            decoration: BoxDecoration(
               color: active
-                  ? GuamColorFamily.grayscaleWhite
-                  : GuamColorFamily.grayscaleGray5,
+                  ? GuamColorFamily.purpleCore
+                  : GuamColorFamily.grayscaleGray7,
+              borderRadius: BorderRadius.all(Radius.circular(16)),
             ),
-          ),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 16,
+                color: active
+                    ? GuamColorFamily.grayscaleWhite
+                    : GuamColorFamily.grayscaleGray5,
+              ),
+            ),
+          )
         )
-      )
+      ),
     );
   }
 }
